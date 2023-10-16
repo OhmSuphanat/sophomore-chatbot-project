292a30aff017ff985fd718c70352dd31a978f9b1
Suphanat <suphanat.sro@ku.th>
2023-10-16 19:17:36 +0700

    load project

---
+++ chatbot/app/Config.py
@@ -0,0 +1,1 @@
+Channel_access_token="I5iFcgIfgTiELZ0Z2jeaQAV3X6y210baCA0GoLDKjNn1yDPaXCFfDhPASsaPX6UQlZVhq3vMAD9BPRIjKiRAB9MRmY/TvgZLXmokfJgqRzLe9OYA6ZYFpqih7wJNepG63f+8cngcQIxs5ltylg87lAdB04t89/1O/w1cDnyilFU="
